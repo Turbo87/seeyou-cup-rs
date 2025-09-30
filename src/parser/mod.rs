ae5c9b01b43bde17b6c371d0cc524527680508ff
Tobias Bieniek <tobias@bieniek.cloud>
2025-10-01 01:06:23 +0200

    Move `ColumnMap` to dedicated module

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,6 +1,9 @@
+mod column_map;
+
 use crate::CupEncoding;
 use crate::CupFile;
 use crate::error::CupError;
+use crate::parser::column_map::{ColumnMap, build_column_map};
 use crate::types::*;
 use csv::StringRecord;
 use encoding_rs::{Encoding, UTF_8, WINDOWS_1252};
@@ -68,77 +71,6 @@ fn parse_content(content: &str) -> Result<CupFile, CupError> {
     let tasks = parse_tasks(&mut csv_iter, &column_map)?;
 
     Ok(CupFile { waypoints, tasks })
-}
-
-struct ColumnMap {
-    name: usize,
-    code: usize,
-    country: usize,
-    lat: usize,
-    lon: usize,
-    elev: usize,
-    style: usize,
-    rwdir: Option<usize>,
-    rwlen: Option<usize>,
-    rwwidth: Option<usize>,
-    freq: Option<usize>,
-    desc: Option<usize>,
-    userdata: Option<usize>,
-    pics: Option<usize>,
-}
-
-fn build_column_map(headers: &StringRecord) -> Result<ColumnMap, String> {
-    let mut name = None;
-    let mut code = None;
-    let mut country = None;
-    let mut lat = None;
-    let mut lon = None;
-    let mut elev = None;
-    let mut style = None;
-    let mut rwdir = None;
-    let mut rwlen = None;
-    let mut rwwidth = None;
-    let mut freq = None;
-    let mut desc = None;
-    let mut userdata = None;
-    let mut pics = None;
-
-    for (idx, header) in headers.iter().enumerate() {
-        match header.to_lowercase().as_str() {
-            "name" => name = Some(idx),
-            "code" => code = Some(idx),
-            "country" => country = Some(idx),
-            "lat" => lat = Some(idx),
-            "lon" => lon = Some(idx),
-            "elev" => elev = Some(idx),
-            "style" => style = Some(idx),
-            "rwdir" => rwdir = Some(idx),
-            "rwlen" => rwlen = Some(idx),
-            "rwwidth" => rwwidth = Some(idx),
-            "freq" => freq = Some(idx),
-            "desc" => desc = Some(idx),
-            "userdata" => userdata = Some(idx),
-            "pics" => pics = Some(idx),
-            _ => {}
-        }
-    }
-
-    Ok(ColumnMap {
-        name: name.ok_or("Missing required column: name")?,
-        code: code.ok_or("Missing required column: code")?,
-        country: country.ok_or("Missing required column: country")?,
-        lat: lat.ok_or("Missing required column: lat")?,
-        lon: lon.ok_or("Missing required column: lon")?,
-        elev: elev.ok_or("Missing required column: elev")?,
-        style: style.ok_or("Missing required column: style")?,
-        rwdir,
-        rwlen,
-        rwwidth,
-        freq,
-        desc,
-        userdata,
-        pics,
-    })
 }
 
 fn parse_waypoints(
